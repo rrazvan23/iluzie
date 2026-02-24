e4efed10e6407d14c6cb1db0a2e8d2e796e8cdc8
rrazvan23 <razvanf.2023@gmail.com>
2026-02-24 13:22:45 +0200

    First commit: site + poze

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/**/*.png',
+    'html/**/*.jpg',
+    'html/**/*.jpeg',
+    'html/**/*.webp',
+    'html/**/*.svg'
+}
